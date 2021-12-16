4add0a7cf4671a966eb076d45d229333b820caf3
andrehsym <kochetkovandre1993@gmail.com>
2021-12-16 22:27:15 +0300

    Готовое ДЗ

--- src/main/java/pro/sky/java/homeworks/course2/homework_7/data/Employee.java
+++ src/main/java/pro/sky/java/homeworks/course2/homework_7/data/Employee.java
@@ -3,20 +3,26 @@ package pro.sky.java.homeworks.course2.homework_7.data;
 import java.util.Objects;
 
 public class Employee {
-    private final String firstName;
-    private final String lastName;
+//    private final String firstName;
+//    private final String lastName;
+    private final String fullName;
+    private String phoneNumber;
 
-    public Employee(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public Employee(String fullName, String phoneNumber) {
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getFullName() {
+        return fullName;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     @Override
@@ -24,20 +30,54 @@ public class Employee {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
-        return firstName.equals(employee.firstName) && lastName.equals(employee.lastName);
+        return fullName.equals(employee.fullName) && phoneNumber.equals(employee.phoneNumber);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(firstName, lastName);
+        return Objects.hash(fullName, phoneNumber);
     }
 
     @Override
     public String toString() {
         return "Employee{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
+                "fullName='" + fullName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
                 '}';
     }
+
+    //    public Employee(String firstName, String lastName) {
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//    }
+
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Employee employee = (Employee) o;
+//        return firstName.equals(employee.firstName) && lastName.equals(employee.lastName);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(firstName, lastName);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Employee{" +
+//                "firstName='" + firstName + '\'' +
+//                ", lastName='" + lastName + '\'' +
+//                '}';
+//    }
 }
 
--- src/main/java/pro/sky/java/homeworks/course2/homework_7/service/impl/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/java/homeworks/course2/homework_7/service/impl/EmployeeServiceImpl.java
@@ -6,46 +6,73 @@ import pro.sky.java.homeworks.course2.homework_7.exceptions.EmployeeAlreadyExist
 import pro.sky.java.homeworks.course2.homework_7.exceptions.EmployeeNotFoundException;
 import pro.sky.java.homeworks.course2.homework_7.service.EmployeeService;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-//    Set employees = new HashSet<>();
-    Set<Employee> employees = new HashSet<>();
+    Map<String, String> employees = new HashMap<>();
 
     @Override
-    public Employee addNew(String firstName, String lastName) {
-        Employee newbie = new Employee(firstName, lastName);
-        if (employees.contains(newbie)) {
+    public Employee addNew(String fullName, String phoneNumber) {
+        Employee newbie = new Employee(fullName, phoneNumber);
+        if (employees.containsKey(fullName) || employees.containsValue(phoneNumber)) {
             throw new EmployeeAlreadyExistsException();
         }
-        employees.add(newbie);
+        employees.put(fullName, phoneNumber);
         return newbie;
     }
 
     @Override
-    public Employee remove(String firstName, String lastName) {
-        Employee removed = new Employee(firstName, lastName);
-        if (!employees.contains(removed)) {
-            throw new EmployeeNotFoundException();
+    public Employee remove(String fullName, String phoneNumber) {
+        Employee removed = new Employee(fullName, phoneNumber);
+        if (employees.containsKey(fullName) && employees.containsValue(phoneNumber)) {
+            employees.remove(fullName);
+            return removed;
         }
-        employees.remove(removed);
-        return removed;
+        throw new EmployeeNotFoundException();
+
+//        if (!employees.containsKey(fullName)) {
+//            throw new EmployeeNotFoundException();
+//        }
+//        employees.remove(fullName);
+//        return removed;
     }
 
     @Override
-    public Employee find(String firstName, String lastName) {
-        Employee find = new Employee(firstName, lastName);
-        if (employees.contains(find)) {
+    public String removeAnEmployeeByKey(String fullName) {
+        if (employees.containsKey(fullName)) {
+            employees.remove(fullName);
+            return fullName;
+        }
+        throw new EmployeeNotFoundException();
+    }
+
+    @Override
+    public Employee find(String fullName, String phoneNumber) {
+        Employee find = new Employee(fullName, phoneNumber);
+        if (employees.containsKey(fullName) && employees.containsValue(phoneNumber)) {
             return find;
         }
         throw new EmployeeNotFoundException();
     }
 
     @Override
-    public Set<Employee> printEmployeeList() {
+    public String findAnEmployeeByKey(String fullName) {
+        if (employees.containsKey(fullName)) {
+            return fullName;
+        }
+        throw new EmployeeNotFoundException();
+    }
+
+    @Override
+    public Map<String, String> printEmployeeList() {
+        for (Map.Entry<String, String> contact: employees.entrySet()) {
+            System.out.println("Сотрудник: " + contact.getKey()+ "; телефон: " + contact.getValue());
+        }
         return employees;
     }
 }
